82cff4f1d1a08b9f5ef7cc4701793e9d735510ee
Ilias <ilias@slascone.com>
2022-09-20 22:22:43 +0200

    extended examples

--- Slascone.Provisioning.Sample.NuGet/Program.cs
+++ Slascone.Provisioning.Sample.NuGet/Program.cs
@@ -3,17 +3,36 @@ using System.Xml;
 
 namespace Slascone.Provisioning.Sample.NuGet;
 
-internal class Program
+ class Program
 {
+    public  ISlasconeClientV2 _slasconeClientV2;
+    public Program() {
+
+        _slasconeClientV2 = new SlasconeClientV2(Helper.ApiBaseUrl,
+                Helper.IsvId,
+                Helper.ProvisioningKey,
+                Helper.SignatureValidationMode,
+                Helper.SymmetricEncryptionKey,
+                Helper.Certificate);
+    }
     static async Task Main(string[] args)
     {
-        ISlasconeClientV2 _slasconeClientV2 = new SlasconeClientV2(Helper.ApiBaseUrl,
-            Helper.IsvId,
-            Helper.ProvisioningKey,
-            Helper.SignatureValidationMode,
-            Helper.SymmetricEncryptionKey,
-            Helper.Certificate);
+        var pr = new Program();
 
+        // ToDo: Uncomment the specific scenario you want to test
+       
+        await ActivationSample(pr._slasconeClientV2);
+        //await HeartbeatSample(pr._slasconeClientV2);
+        //await AnalyticalHeartbeatSample(pr._slasconeClientV2);
+        //await UsageHeartbeatSample(pr._slasconeClientV2);
+        //await ConsumptionHeartbeatSample(pr._slasconeClientV2);
+        //await OpenSession(pr._slasconeClientV2);
+        //await CloseSession(pr._slasconeClientV2);
+        //LicenseFileSample("XX/LicenseSample.xml");
+    }
+
+    private static async Task ActivationSample(ISlasconeClientV2 _slasconeClientV2)
+    {
         var activateClientDto = new ActivateClientDto
         {
             Product_id = Guid.Parse("b18657cc-1f7c-43fa-e3a4-08da6fa41ad3"),
@@ -26,14 +45,17 @@ internal class Program
 
         try
         {
-            var activatedLicense = await _slasconeClientV2.ActivateLicenseAsync(activateClientDto);
-
+            var result = await _slasconeClientV2.ActivateLicenseAsync(activateClientDto);
             /*   If the activation failed, the api server responses with a specific error message which describes 
                  the problem. Therefore the LicenseInfo object is declared with null. */
 
-            if (activatedLicense.StatusCode == "409")
+            if (result.StatusCode == "200")
             {
-                Console.WriteLine(activatedLicense.Error.Message);
+                Console.WriteLine("Successfull activation.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
                 /*Example for 
                 if (activatedLicense.Error.Id == 2006)
                 { 
@@ -42,119 +64,95 @@ internal class Program
             }
             else
             {
-                Console.WriteLine("Successfully activated license.");
+                Console.WriteLine(result.Message);
             }
         }
-        catch(Exception ex)
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    private static async Task HeartbeatSample(ISlasconeClientV2 _slasconeClientV2)
+    {
+ 
+        // ToDo: Fill the variables
+        var heartbeatDto = new AddHeartbeatDto
+        {
+            //Token_key = Guid.Parse(""),
+            Product_id = Guid.Parse("b18657cc-1f7c-43fa-e3a4-08da6fa41ad3"),
+            Client_id = Helper.GetWindowsUniqueDeviceId(),
+            Software_version = "22.1",
+            //GroupId = "",
+            //HeartbeatTypeId = Guid.Parse(""),
+            Operating_system = Helper.GetOperatingSystem()
+        };
+
+        try
+        {
+            var result = await _slasconeClientV2.AddHeartbeatAsync(heartbeatDto);  
+            if (result.StatusCode == "200")
+            {
+                Console.WriteLine("Successfully created heartbeat.");
+            }
+            else if(result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
         }
 
-        Console.ReadLine();
-
-        // ToDo: Uncomment specific scenario
-        //await FloatingLicensingSample(activatedLicense);
-        //await HeartbeatSample(activatedLicense);
-        //LicenseFileSample("XX/LicenseSample.xml");
     }
 
-    private async Task FloatingLicensingSample(LicenseInfoDto activatedLicense)
+    private async Task AnalyticalHeartbeatSample(ISlasconeClientV2 _slasconeClientV2)
     {
-        ISlasconeClientV2 _slasconeClientV2 = new SlasconeClientV2(Helper.ApiBaseUrl,
-            Helper.IsvId,
-            Helper.ProvisioningKey,
-            Helper.SignatureValidationMode,
-            Helper.SymmetricEncryptionKey,
-            Helper.Certificate);
-
-        // ToDo: Fill the variables
-        var heartbeatDto = new AddHeartbeatDto
-        {
-
-            Product_id = Guid.Parse(""),
-            Client_id = Helper.GetWindowsUniqueDeviceId(),
-            Software_version = "",
-            //GroupId = "",
-            //HeartbeatTypeId = Guid.Parse(""),
-            //TokenKey = "",
-            Operating_system = Helper.GetOperatingSystem()
-        };
-
-        var heartbeatResult = await _slasconeClientV2.AddHeartbeatAsync(heartbeatDto);
-
-        /* After successfully generating a heartbeat the client have to check provisioning mode of the license.
-           Is it floating a session has to be opened. */
-        if (heartbeatResult != null && heartbeatResult.Result?.Provisioning_mode == ProvisioningMode.Floating)
-        {
-            // ToDo: Fill the variables
-            var sessionDto = new SessionRequestDto
-            {
-                Client_id = Helper.GetWindowsUniqueDeviceId(),
-                License_id = Guid.Parse("")
-            };
-
-            var openSessionResult = await _slasconeClientV2.OpenSessionAsync(sessionDto);
-
-            //If the floating limit is reached the api server responses with an Error.
-            if (openSessionResult.StatusCode == "409")
-            {
-                Console.WriteLine(openSessionResult.Error?.Message);
-            }
-            else
-            {
-                Console.WriteLine("Session active until: " + openSessionResult.Result?.Session_valid_until);
-            }
-
-            /* If the client have finished his work, he has to close the session.
-               Therefore other clients are not blocked anymore and have not to wait until another Client expired. */
-            var closeSessionResult = await _slasconeClientV2.CloseSessionAsync(sessionDto);
-
-            Console.WriteLine(closeSessionResult);
-        }
-    }
-
-    private async Task HeartbeatSample(LicenseInfoDto activatedLicense)
-    {
-        ISlasconeClientV2 _slasconeClientV2 = new SlasconeClientV2(Helper.ApiBaseUrl,
-            Helper.IsvId,
-            Helper.ProvisioningKey,
-            Helper.SignatureValidationMode,
-            Helper.SymmetricEncryptionKey,
-            Helper.Certificate);
-
-        // ToDo: Fill the variables
-        var heartbeatDto = new AddHeartbeatDto
-        {
-            Token_key = Guid.Parse(""),
-            Product_id = Guid.Parse(""),
-            Client_id = Helper.GetWindowsUniqueDeviceId(),
-            Software_version = "",
-            //GroupId = "",
-            //HeartbeatTypeId = Guid.Parse(""),
-            Operating_system = Helper.GetOperatingSystem()
-        };
-
-        var heartbeatResult = await _slasconeClientV2.AddHeartbeatAsync(heartbeatDto);
-
-        /* If the heartbeat failed, the api server responses with a specific error message which describes the problem.
-           Therefore the LicenseInfo object is declared with null. */
-        if (heartbeatResult.StatusCode == "409")
-        {
-            Console.WriteLine(heartbeatResult.Error?.Message);
-        }
-        else
-        {
-            Console.WriteLine("Successfully created heartbeat.");
-        }
 
         // ToDo: Fill the variables
         var analyticalHb = new AnalyticalHeartbeatDto();
         analyticalHb.Analytical_heartbeat = new List<AnalyticalFieldValueDto>();
         analyticalHb.Client_id = Helper.GetWindowsUniqueDeviceId();
 
-        var analyticalHeartbeatResult = await _slasconeClientV2.AddAnalyticalHeartbeatAsync(analyticalHb);
+        try
+        {
+            var result = await _slasconeClientV2.AddAnalyticalHeartbeatAsync(analyticalHb);
+            if (result.StatusCode == "200")
+            {
+                Console.WriteLine("Successfully created analytical heartbeat.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 
-        Console.WriteLine(analyticalHeartbeatResult);
+    private async Task UsageHeartbeatSample(ISlasconeClientV2 _slasconeClientV2)
+    {
+
 
         // ToDo: Fill the variables
         var usageHeartbeat = new FullUsageHeartbeatDto();
@@ -171,22 +169,35 @@ internal class Program
         usageHeartbeat.Usage_heartbeat.Add(usageFeatureValue1);
         usageHeartbeat.Usage_heartbeat.Add(usageFeatureValue2);
 
-        var usageHeartbeatResult = await _slasconeClientV2.AddUsageHeartbeatAsync(usageHeartbeat);
-
-        Console.WriteLine(usageHeartbeatResult);
-
-        if (activatedLicense != null)
+        try
         {
-            // ToDo: Fill the variables
-            var unassignDto = new UnassignDto
+            var result = await _slasconeClientV2.AddUsageHeartbeatAsync(usageHeartbeat);
+            if (result.StatusCode == "200")
             {
-                Token_key = Guid.Parse("")
-            };
-
-            var unassignResult = await _slasconeClientV2.UnassignLicenseAsync(unassignDto);
-
-            Console.WriteLine(unassignResult);
+                Console.WriteLine("Successfully created usage heartbeat.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private async Task ConsumptionHeartbeatSample(ISlasconeClientV2 _slasconeClientV2)
+    {
 
         // ToDo: Fill the variables
         var consumptionHeartbeat = new FullConsumptionHeartbeatDto();
@@ -200,13 +211,110 @@ internal class Program
         consumptionHeartbeatValue1.Limitation_id = Guid.Parse("");
         consumptionHeartbeat.Consumption_heartbeat.Add(consumptionHeartbeatValue1);
 
-        var consumptionHeartbeatResult = await _slasconeClientV2.AddConsumptionHeartbeatAsync(consumptionHeartbeat);
+        try
+        {
+            var result = await _slasconeClientV2.AddConsumptionHeartbeatAsync(consumptionHeartbeat);
 
-        Console.WriteLine(consumptionHeartbeatResult);
-
-        var remainingConsumptions = await _slasconeClientV2.GetConsumptionStatusAsync(new ValidateConsumptionStatusDto
-        { Limitation_id = Guid.Parse(""), Client_id = Helper.GetWindowsUniqueDeviceId() });
+            if (result.StatusCode == "200")
+            {
+                Console.WriteLine("Successfully created consumption heartbeat.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
+
+
+    private static async Task OpenSession(ISlasconeClientV2 _slasconeClientV2)
+    {
+        var sessionDto = new SessionRequestDto
+        {
+            Client_id = Helper.GetWindowsUniqueDeviceId(),
+            License_id = Guid.Parse("27180460-29df-4a5a-a0a1-78c85ab6cee0")
+        };
+
+        try
+        {
+            var result = await _slasconeClientV2.OpenSessionAsync(sessionDto);
+            if (result.StatusCode == "200")
+            {
+                Console.WriteLine("Successfully opened session.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+
+    private static async Task CloseSession(ISlasconeClientV2 _slasconeClientV2)
+    {
+        var sessionDto = new SessionRequestDto
+        {
+            Client_id = Helper.GetWindowsUniqueDeviceId(),
+            License_id = Guid.Parse("27180460-29df-4a5a-a0a1-78c85ab6cee0")
+        };
+
+        try
+        {
+            var result = await _slasconeClientV2.CloseSessionAsync(sessionDto);
+            if (result.StatusCode == "200")
+            {
+                Console.WriteLine("Successfully closed session.");
+            }
+            else if (result.StatusCode == "409")
+            {
+                Console.WriteLine(result.Error.Message);
+                /*Example for 
+                if (activatedLicense.Error.Id == 2006)
+                { 
+                }
+                */
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+
+
 
     private static bool LicenseFileSample(string licenseFile)
     {
